e03f258b643564ded790cb20014b9705e0d1a77a
Vijay reddy <padalavijayreddy@gmail.com>
2020-06-11 10:38:17 +0000

    ProjectThree

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProjectThree extends React.Component {
+    render() {
+        return (
+            <div>
+                """.....HELLO THREE....."""
+            </div>
+        );
+    }
+}
+
+export { ProjectThree };
